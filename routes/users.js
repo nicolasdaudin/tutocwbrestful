d017d4d3f9379e86425e1d785878b944cb544d38
Nicolas Daudin <nicolas.daudin@gmail.com>
2014-08-25 14:30:35 -0500

    Restful tutorial finished.

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,34 @@ var router = express.Router();
  */
 router.get('/userlist', function(req, res) {
     var db = req.db;
+    console.log('accessing userlist....');
     db.collection('userlist').find().toArray(function (err, items) {
+    	console.log('items found in the userlist: ' + JSON.stringify(items));
         res.json(items);
     });
+});
+
+/*
+ * POST to adduser
+ */
+router.post('/adduser',function(req,res){
+	var db=req.db;
+	db.collection('userlist').insert(req.body,function(err,result){
+		res.send(
+			(err === null) ? {msg:''}:{msg:err}
+		);
+	});
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req,res){
+	var db = req.db;
+	var userToDelete = req.params.id;
+	db.collection('userlist').removeById(userToDelete,function(err,result){
+		res.send((result===1) ? {msg : '' } : {msg:'error: ' + err});
+	});
 });
 
 module.exports = router;
